c6846c92e3d8b7df111f5547a1dcda95d0556fd8
kunkka <buxingmengzhong@163.com>
2017-08-15 15:59:06 +0800

    markdown editor

---
+++ src/base/markdown/markdown.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="markdown-content">
+    <div class="markdown markdown-body">
+      <textarea name="markdown" id="markdown"></textarea>
+    </div>
+  </div>
+</template>
+
+<script>
+  import SimpleMDE from 'simplemde'
+
+  export default{
+    data() {
+      return {}
+    },
+    created() {
+      this.$nextTick(() => {
+        /* eslint-disable no-unused-vars */
+        this.simplemde = new SimpleMDE({
+          element: document.getElementById('markdown'),
+          autoDownloadFontAwesome: false,
+          status: false
+//          toolbar: ['bold', 'italic', 'strikethrough', '|', 'heading-1', 'heading-2', 'heading-3', '|', 'quote', 'table']
+        })
+      })
+    },
+    computed: {},
+    methods: {
+      getHtml() {
+        console.log(this.simplemde)
+        let testPlain = this.simplemde.value()
+        let textMarkdown = this.simplemde.markdown(testPlain)
+        console.log(textMarkdown)
+      }
+    },
+    components: {}
+  }
+</script>
+
+<style rel="stylesheet/scss" lang="scss">
+  .markdown-content {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    width: 97%;
+    padding: 20px;
+    justify-content: center;
+    align-items: center;
+    .markdown, .effect {
+      flex: 1;
+      height: 100%;
+    }
+    .markdown {
+      .editor-toolbar {
+        border-top-right-radius: 0;
+      }
+      .CodeMirror {
+        height: 90%;
+      }
+    }
+  }
+</style>
---
+++ src/components/index/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <div class="button" @click="getHtml">获取HTML</div>
+    <markdown ref="markdown"></markdown>
+  </div>
+</template>
+
+<script>
+  import Markdown from 'base/markdown/markdown'
+
+  export default{
+    props: {},
+    data() {
+      return {}
+    },
+    computed: {},
+    methods: {
+      getHtml() {
+        this.$refs.markdown.getHtml()
+      }
+    },
+    components: {
+      Markdown
+    }
+  }
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  .container {
+    position: relative;
+    .button {
+      position: absolute;
+      top: 0;
+      left: 0;
+      z-index: 99999999;
+    }
+  }
+</style>
